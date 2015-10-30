babc29769cd14c8b650fcc232a392f930564b358
Richard von Rauchhaupt (Rover) <Richard.vonRauchhaupt@stonybrook.edu>
2015-10-30 11:05:06 -0400

    tab

--- public/js/footer/footer.dev.js
+++ public/js/footer/footer.dev.js
@@ -8499,11 +8499,7 @@ if( !Tablesaw.config ) {
 }).call(this);
 (function($) {
 
-
-
     $(document).ready(function() {
-
-
 
         var accordionContent;
         var openTab;
@@ -8530,7 +8526,92 @@ if( !Tablesaw.config ) {
             }
 
         });
+        
+       activeTab = $('.nav-tabs-wrapper li.active');
+       console.log(activeTab);
+
+       if(activeTab.length === 0 )
+       {
+         activeTab = $('.nav-tabs-wrapper li').first();
+         console.log('activetab');
+         console.log(activeTab);
+         activeTab.addClass('active');
+
+         activeTabContent = $('.nav-tabs-wrapper .tab-content .tab-pane');
+         activeTabContent.first().addClass('active');
+         console.log(activeTabContent);
+       }
 
     });
+
+
+$(document).ready(function(){        
+    $('.image-gallery li img').on('click',function(){
+        console.log("image click");
+        
+        var src = $(this).attr('src');
+        var img = '<img src="' + src + '" class="img-responsive"/>';
+        
+        //start of new code new code
+        var index = $(this).parent('li').index();   
+        
+        var html = '';
+        html += img;                
+        html += '<div style="height:25px;clear:both;display:block;">';
+        html += '<a class="controls next" href="'+ (index+2) + '">next &raquo;</a>';
+        html += '<a class="controls previous" href="' + (index) + '">&laquo; prev</a>';
+        html += '</div>';
+        
+        $('#myModal').modal();
+        $('#myModal').on('shown.bs.modal', function(){
+            $('#myModal .modal-body').html(html);
+            //new code
+            $('a.controls').trigger('click');
+        })
+        $('#myModal').on('hidden.bs.modal', function(){
+            $('#myModal .modal-body').html('');
+        });
+        
+        
+        
+        
+   });  
+})
+        
+         
+$(document).on('click', 'a.controls', function(){
+    var index = $(this).attr('href');
+    var src = $('ul.row li:nth-child('+ index +') img').attr('src');             
+    
+    $('.modal-body img').attr('src', src);
+    
+    var newPrevIndex = parseInt(index) - 1; 
+    var newNextIndex = parseInt(newPrevIndex) + 2; 
+    
+    if($(this).hasClass('previous')){               
+        $(this).attr('href', newPrevIndex); 
+        $('a.next').attr('href', newNextIndex);
+    }else{
+        $(this).attr('href', newNextIndex); 
+        $('a.previous').attr('href', newPrevIndex);
+    }
+    
+    var total = $('ul.row li').length + 1; 
+    //hide next button
+    if(total === newNextIndex){
+        $('a.next').hide();
+    }else{
+        $('a.next').show()
+    }            
+    //hide previous button
+    if(newPrevIndex === 0){
+        $('a.previous').hide();
+    }else{
+        $('a.previous').show()
+    }
+    
+    
+    return false;
+});
 
 })(jQuery);
