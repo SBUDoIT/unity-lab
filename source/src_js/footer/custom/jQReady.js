63eacc0abdd4fd85948c30c8fdc7519a09521375
Richard von Rauchhaupt @ DOIT-COMM-01 <Richard.VonRauchhaupt@stonybrook.edu>
2016-05-20 19:35:43 -0400

    Refactored the new event-button to farbeyond-event-button added support for background-image-card sections refactored image gallery modal to follow BEM removed/excluded unused sass files and templates moved unknown items to homeless folder

--- source/src_js/footer/custom/jQReady.js
+++ source/src_js/footer/custom/jQReady.js
@@ -46,38 +46,7 @@
     });
 
 
-$(document).ready(function(){
-    $('.image-gallery li img').on('click',function(){
-      //  console.log("image click");
 
-        var src = $(this).attr('src');
-        var img = '<img src="' + src + '" class="img-responsive"/>';
-
-        //start of new code new code
-        var index = $(this).parent('li').index();
-
-        var html = '';
-        html += img;
-        html += '<div style="height:25px;clear:both;display:block;">';
-        html += '<a class="controls next" href="'+ (index+2) + '">next &raquo;</a>';
-        html += '<a class="controls previous" href="' + (index) + '">&laquo; prev</a>';
-        html += '</div>';
-
-        $('#myModal').modal();
-        $('#myModal').on('shown.bs.modal', function(){
-            $('#myModal .modal-body').html(html);
-            //new code
-            $('a.controls').trigger('click');
-        })
-        $('#myModal').on('hidden.bs.modal', function(){
-            $('#myModal .modal-body').html('');
-        });
-
-
-
-
-   });
-})
 
 
 $(document).on('click', 'a.controls', function(){
