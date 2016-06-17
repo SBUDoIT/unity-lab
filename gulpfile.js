9491a6ec805afbb4b774d4d6c1ee4aef29edd167
Richard von Rauchhaupt @ DOIT-COMM-01 <Richard.VonRauchhaupt@stonybrook.edu>
2016-06-17 14:30:56 -0400

    gulp changes for automating releases

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,68 @@ var bump = require("gulp-bump");
 var config = require('./gulp.config.json');
 var localconfig = require('./gulp.config.local.json');
 var shell = require('gulp-shell');
+var buildArgs = require('./gulp.build.args.js');
+
+
+gulp.task('bump', function () {
+  return gulp.src(['./package.json'])
+    .pipe(bump({type: buildArgs.versionBumpType}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+  return gulp.src('.')
+    .pipe(git.add())
+    .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+  git.push('origin', 'master', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+  var version = getPackageJsonVersion();
+  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+    if (error) {
+      return cb(error);
+    }
+    git.push('origin', 'master', {args: '--tags'}, cb);
+  });
+
+  function getPackageJsonVersion () {
+    // We parse the json file instead of using require because require caches
+    // multiple calls so the version number won't be updated
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  };
+});
+
+
+gulp.task('release', function (callback) {
+  runSequence(
+    'bump',
+//    'changelog',
+    'commit-changes',
+    'push-changes',
+    'create-new-tag',
+//    'github-release',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
+});
+
+/*
+gulp.task('copy:release', function() {
+    var pkg = require('./package.json');
+    gulp.src(config.release.src)
+        .pipe(gulp.dest(config.release.dest + "/" + pkg.version));
+
+});*/
+
 
 
 gulp.task('copy:projects', ['clean:projects'], function() {
